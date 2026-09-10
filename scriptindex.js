0c7c0c8dc2e7058ed8b249a9aa670681f4bbebae
Cadu Dantas <cadudantasgja@icloud.com>
2026-09-10 19:54:29 -0300

    Primeiro commit

---
+++ scriptindex.js
@@ -0,0 +1,64 @@
+annistates.style.display = "none"
+
+let titulodiferenciado = document.getElementById("titulodiferenciado")
+
+    titulodiferenciado.onclick = function () {
+
+     celestiais.style.display = "none";
+
+     annistates.style.display = "block";
+
+    }
+
+let titulodiferenciado1 = document.getElementById("titulodiferenciado1")
+
+    titulodiferenciado1.onclick = function () {
+
+     celestiais.style.display = "block";
+
+     annistates.style.display = "none";
+
+    }
+
+let index = document.getElementById("index")
+
+   index.onclick = function() {
+
+        window.location.href = "index.html";
+
+    }    
+
+let  historia = document.getElementById("historia")
+
+    historia.onclick = function() {
+
+        window.location.href = "historia.html";
+
+    }
+
+let  catalogo = document.getElementById("catalogo")
+
+    catalogo.onclick = function() {
+
+        window.location.href = "catalogo.html";
+
+    }
+
+let  OJ = document.getElementById("OJ")
+
+    OJ.onclick = function() {
+
+        window.location.href = "ot.html";
+
+    }
+
+let  creditos = document.getElementById("creditos")
+
+    creditos.onclick = function() {
+        
+        window.location.href = "creditos.html";
+
+    }
+
+
+
